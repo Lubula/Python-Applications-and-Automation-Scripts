d2eabb3b7f304f68afdffd3be972731842ad7849
Lubula Paul Chikwekwe <64841198+Lubula@users.noreply.github.com>
2023-12-05 22:03:54 +0200

    Update 7_basic_calculator.py
    complete code write to improve

--- 7_basic_calculator.py
+++ 7_basic_calculator.py
@@ -1,18 +1,19 @@
-def add(a, b):
-    ans = a + b
-    print(str(a), "+", str(b), "=", str(ans))
+def get_number_input(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
 
-def sub(a, b):
-    ans = a - b
-    print(str(a), "-", str(b), "=", str(ans))
-
-def mult(a, b):
-    ans = a * b
-    print(str(a), "*", str(b), "=", str(ans))
-
-def div(a, b):
-    ans = a / b
-    print(str(a), "/", str(b), "=", str(ans))
+def perform_operation(operation, a, b):
+    if operation == "a":
+        add(a, b)
+    elif operation == "b":
+        sub(a, b)
+    elif operation == "c":
+        mult(a, b)
+    elif operation == "d":
+        div(a, b)
 
 def main():
     while True:
@@ -20,29 +21,41 @@ def main():
 
         choice = input("Enter your choice (A/B/C/D/E): ").lower()
 
-        if choice == "a":
-            print("Addition")
-            a = int(input("Input your first number: "))
-            b = int(input("Input your second number: "))
-            add(a,b)
-        elif choice == "b":
-            print("Subtraction")
-            a = int(input("Input your first number: "))
-            b = int(input("Input your second number: "))
-            sub(a,b)
-        elif choice == "c":
-            print("Multiplication")
-            a = int(input("Input your first number: "))
-            b = int(input("Input your second number: "))
-            mult(a,b)
-        elif choice == "d":
-            print("Division")
-            a = int(input("Input your first number: "))
-            b = int(input("Input your second number: "))
-            div(a,b)
-        elif choice == "e":
+        if choice == "e":
             print("Program Ended")
             quit()
+
+        if choice not in {"a", "b", "c", "d"}:
+            print("Invalid choice. Please enter A, B, C, D, or E.")
+            continue
+
+        print(f"{choice.upper()}. {get_operation_name(choice)}")
+        a = get_number_input("Input your first number: ")
+        b = get_number_input("Input your second number: ")
+        perform_operation(choice, a, b)
+
+def get_operation_name(operation):
+    operation_names = {"a": "Addition", "b": "Subtraction", "c": "Multiplication", "d": "Division"}
+    return operation_names.get(operation)
+
+def add(a, b):
+    ans = a + b
+    print(f"{a} + {b} = {ans}")
+
+def sub(a, b):
+    ans = a - b
+    print(f"{a} - {b} = {ans}")
+
+def mult(a, b):
+    ans = a * b
+    print(f"{a} * {b} = {ans}")
+
+def div(a, b):
+    if b == 0:
+        print("Cannot divide by zero.")
+    else:
+        ans = a / b
+        print(f"{a} / {b} = {ans}")
 
 if __name__ == '__main__':
     main()
