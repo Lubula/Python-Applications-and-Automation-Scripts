243d7e84e909485242fdb8a810e4a5b7b9e34e78
Lubula Paul Chikwekwe <64841198+Lubula@users.noreply.github.com>
2023-12-05 21:58:02 +0200

    Update 6_email_slicer.py
    more concise and add flexibility to reduce errors.

--- 6_email_slicer.py
+++ 6_email_slicer.py
@@ -8,17 +8,25 @@ def email_slicer():
 
     # Collect the email address from the user
     email_input = input("Input your email address: ")
-    
+
     # Split the email into username and domain parts
-    (username,domain) = email_input.split("@")
-    
+    if "@" not in email_input or "." not in email_input:
+        print("Invalid email address. Please enter a valid email.")
+        return
+
+    username, domain = email_input.split("@")
+
     # Split the domain into domain name and extension parts
-    (domain_name, extension) = domain.split(".")
+    if "." not in domain:
+        print("Invalid domain in email address. Please enter a valid email.")
+        return
+
+    domain_name, extension = domain.split(".")
 
     # Print out the username, domain name, and extension
-    print("Username: ", username)
-    print("Domain: ", domain_name)
-    print("Extension: ", extension)
+    print(f"Username: {username}")
+    print(f"Domain: {domain_name}")
+    print(f"Extension: {extension}")
 
 # Call the function to run the code
 email_slicer()
