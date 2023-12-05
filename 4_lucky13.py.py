a3f417fd0bf480680363572f63c811dd573d3087
Lubula Paul Chikwekwe <64841198+Lubula@users.noreply.github.com>
2023-12-05 21:51:47 +0200

    Update 4_lucky13.py.py
    more concise, flexible and robust

--- 4_lucky13.py.py
+++ 4_lucky13.py.py
@@ -2,30 +2,30 @@ import random
 
 def play_lucky13():
     print("Welcome to Lucky13!")
-    
-    r_number = random.randint(1, 13) # using random.randint to generate random integers between 1 and 13
-    
-    attempt = 0
+
+    random_number = random.randint(1, 13)
+    attempts = 0
     
     while True:
-        attempt += 1
+        attempts += 1
         user_guess = input("Make a guess from 1-13: ")
-        
-        if user_guess.isdigit() and 1 <= int(user_guess) <= 13: # checking if the user input is a digit between 1 and 13
-            user_guess = int(user_guess)
+
+        # Checking if the user input is a digit between 1 and 13
+        if user_guess.isdigit() and 1 <= (user_guess := int(user_guess)) <= 13:
+            pass
         else:
-            print("Invalid input. Please try again.")
+            print("Invalid input. Please enter a digit between 1 and 13.")
             continue
-        
-        if user_guess == r_number:
+
+        if user_guess == random_number:
             print("Congratulations! You got it!")
             break 
         else:
-            if user_guess > r_number:
+            if user_guess > random_number:
                 print("Too high.")
             else:
                 print("Too low.")
     
-    print("You got it in", attempt, "attempts!")
+    print("You got it in", attempts, "attempts!")
 
 play_lucky13()
