58cc2ed1dcbb240f48c0c3dac8ecee99c9ca5d51
Lubula Paul Chikwekwe <64841198+Lubula@users.noreply.github.com>
2023-12-05 21:54:37 +0200

    Update 5_word_replacer.py
    still simple code, simply added more comments and reduced errors. removed double use o same function

--- 5_word_replacer.py
+++ 5_word_replacer.py
@@ -1,9 +1,23 @@
 def word_replacer():
+    # Sample input text
     input_text = "Nsansa Inc and Nsansa Homes"
-    word_to_replace, word_replacement = input("Enter the word to replace and the replacement word separated by a comma: ").split(",")
-    print(f"The result of the replacement is: {input_text.replace(word_to_replace, word_replacement)}")
+    
+    # Get user input for word to replace and replacement word
+    user_input = input("Enter the word to replace and the replacement word separated by a comma: ")
+
+    # Validate user input
+    if "," not in user_input:
+        print("Invalid input. Please provide both the word to replace and the replacement word separated by a comma.")
+        return
+
+    # Split the user input into the word to replace and the replacement word
+    word_to_replace, word_replacement = user_input.split(",")
+    
+    # Perform the replacement and print the result
+    # Using replace with max replacement set to 1 to replace only the first occurrence
+    replaced_text = input_text.replace(word_to_replace, word_replacement, 1)
+    print(f"The result of the replacement is: {replaced_text}")
 
 if __name__ == '__main__':
+    # Call the word_replacer function when the script is run
     word_replacer()
-
-word_replacer()
