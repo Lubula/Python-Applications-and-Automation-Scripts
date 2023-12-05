c52885a698136a4cf1eecbe143ab0207295bc421
Lubula Paul Chikwekwe <64841198+Lubula@users.noreply.github.com>
2023-12-05 21:46:55 +0200

    Update 3_rock,_paper_scissors.py
    improved code made cleaner and more flexible and robust.

--- 3_rock,_paper_scissors.py
+++ 3_rock,_paper_scissors.py
@@ -1,47 +1,34 @@
-# import the necessary module
 import random
 
-# initialize variables for user and computer wins
-user_wins = 0
-computer_wins = 0
-
-# create a list of valid options
-options = ["rock", "paper", "scissors"]
-
-# loop until the user quits the game
 while True:
-    # get user input and convert to lowercase
-    user_input = input("Type Rock/Paper/Scissors or Q (to quit): ").lower()
+    user_wins = 0
+    computer_wins = 0
+    options = ["rock", "paper", "scissors"]
 
-    # check if the user wants to quit
-    if user_input == "q":
-        break
+    while True:
+        user_input = input("Type Rock/Paper/Scissors or Q (to quit): ").lower()
 
-    # check if the user input is valid
-    if user_input not in options:
-        continue
+        if user_input == "q":
+            break
 
-    # generate a random number to pick a play for the computer
-    computer_pick = random.choice(options)
-    print("Computer picked", computer_pick + ".")
+        if user_input not in options:
+            continue
 
-    # determine the winner of the round based on the user and computer picks
-    if user_input == computer_pick:
-        print("Both players picked the same. Play again!")
-    elif user_input == "rock" and computer_pick == "scissors":
-        print("You win!")
-        user_wins += 1
-    elif user_input == "paper" and computer_pick == "rock":
-        print("You win!")
-        user_wins += 1
-    elif user_input == "scissors" and computer_pick == "paper":
-        print("You win!")
-        user_wins += 1
-    else:
-        print("Computer wins!")
-        computer_wins += 1
+        computer_pick = random.choice(options)
+        print(f"Computer picked {computer_pick}.")
 
-# print the final scores and exit the game
-print("You won", user_wins, "times.")
-print("The computer won", computer_wins, "times.")
-print("Goodbye!")
+        if user_input == computer_pick:
+            print("Both players picked the same. Play again!")
+        elif (user_input == "rock" and computer_pick == "scissors") or \
+             (user_input == "paper" and computer_pick == "rock") or \
+             (user_input == "scissors" and computer_pick == "paper"):
+            print("You win!")
+            user_wins += 1
+        else:
+            print("Computer wins!")
+            computer_wins += 1
+
+    print(f"You won {user_wins} times.")
+    print(f"The computer won {computer_wins} times.")
+    print("Goodbye!")
+    break
