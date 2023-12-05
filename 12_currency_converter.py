4f4b025ffba605589b260c9ed57cfdf35b86c862
Lubula Paul Chikwekwe <64841198+Lubula@users.noreply.github.com>
2023-12-05 21:32:06 +0200

    Update 12_currency_converter.py
    better comments and more flexible code for decimals in financial values

--- 12_currency_converter.py
+++ 12_currency_converter.py
@@ -1,15 +1,18 @@
-# program that converts us dollers to british pounds
-# use a lambda function
-# can add an api to add all the currency that can be called 
-
 def main():
-    print("This Program Converts US Dollers To Pounds Sterling")
+    print("This Program Converts US Dollars To Pounds Sterling")
     print("")
 
-    dollars = eval(input("Enter Amount In Dollers: "))
-    pounds = convert_to_pounds(dollars)
-    print("That is", pounds, "Pounds")
+    # Prompt the user to enter an amount in dollars
+    dollars = float(input("Enter Amount In Dollars: "))  # Using float() instead of eval() for safer input parsing
 
+    # Call the convert_to_pounds function to calculate the equivalent amount in pounds
+    pounds = convert_to_pounds(dollars)
+
+    # Display the result with two decimal places for better clarity
+    print("That is {:.2f} Pounds".format(pounds))  # Formatting the output to two decimal places
+
+# A lambda function that takes dollars as input and returns the equivalent amount in pounds
 convert_to_pounds = lambda dollars: dollars * 0.82
 
+# Call the main function to execute the program
 main()
