18bb8ac38b3bb9c8bee8c001d0320ca41c235584
Lubula Paul Chikwekwe <64841198+Lubula@users.noreply.github.com>
2023-12-05 21:40:57 +0200

    Update 1_choice_game.py
    added more functions and also more functions to improve usability

--- 1_choice_game.py
+++ 1_choice_game.py
@@ -8,7 +8,7 @@ if age >= 14:
     wants_to_play = input("Do you want to play? (yes/no) ").lower()
 
     if wants_to_play == "yes":
-        print("Welcome,", name, ". You are", age, "years old. I wonder how mature you are? Your level of intuition is at", intuition, "%.")
+        print(f"Welcome, {name}. You are {age} years old. I wonder how mature you are? Your level of intuition is at {intuition}%.")
         print("Let's begin!")
 
         first_choice = input("What do you depend on to make a decision? Head or Heart? (head/heart) ").lower()
@@ -21,19 +21,19 @@ if age >= 14:
             if ans == "identify":
                 print("Recognizing that something is not as it should be is the key first step.")
                 intuition += 15
-            
-            elif ans == "define": 
+
+            elif ans == "define":
                 print("Clearly understanding the nature of the issue, its scope, and its underlying causes is important.")
                 intuition += 5
 
             ans = input("Now gather information & evaluate options. Do you then take action or make a decision? (action/decide) ").lower()
 
-            if ans == "decide": 
+            if ans == "decide":
                 print("Making a decision is the key. You're gaining intuition!")
                 intuition += 15
 
                 if intuition >= 30:
-                    print("Your intuition level is at", intuition, "%. Well done!")
+                    print(f"Your intuition level is at {intuition}%. Well done!")
                 else:
                     print("Your intuition level is less than 30%. GAME OVER <RESTART>")
 
@@ -44,15 +44,19 @@ if age >= 14:
                 if intuition <= 30:
                     print("Your intuition level is less than 30%. GAME OVER <RESTART>")
                 else:
-                    print("Your intuition level is at", intuition, "%. Well done!")
+                    print(f"Your intuition level is at {intuition}%. Well done!")
+
             else:
                 print("You're lost now. You can't take action without making a decision first. Critical error! GAME OVER <RESTART>")
 
         else:
             print("Trust your intuition and follow your heart more. Leave the data crunching to the calculators. GAME OVER <RESTART>")
 
-    else:
+    elif wants_to_play == "no":
         print("BYE")
+
+    else:
+        print("Invalid response. Please enter 'yes' or 'no'. BYE")
 
 else:
     print("Still too young to play!")
